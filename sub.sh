c5fa222b9448116b8e9895730de588c3d823939c
Brian L. Chen <brian.lxchen@gmail.com>
2021-12-10 02:27:12 +0800

    Add zip option

--- sub.sh
+++ sub.sh
@@ -3,4 +3,15 @@
 # + [ ] Binary 執行檔(已 Compile 及 Linking 完成並可執行的 Client 端程 式)。
 # + [ ] 用以編譯程式之 Makefile。請將上述四項檔案壓縮成:
 # + 學號_part1.tar.gz (e.g. b027050xx_part1.tar.gz)，
-tar -czf $1_part1.tar.gz docs/phase01-doc.pdf src/ client Makefile
+
+FILES="docs/phase01-doc.pdf src/ client Makefile"
+
+if [ "$1" = "tar" ]
+then
+    tar -czf $2_part1.tar.gz $FILES
+elif [ "$1" = "zip" ]
+then
+    zip $2_part1.zip $FILES
+else
+    echo "please specify zip or tar"
+fi
